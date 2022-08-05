24c5a7c4fab5efcdb363c22301d2388948a20dd2
unknown <qatrelnadasouka@gmail.com>
2022-08-05 05:17:52 +0200

    footer component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../css/Footer/Footer.css"
+
+function Footer() {
+    return ( 
+        <footer>
+            &copy; Copyright Qatr El-Nada Souka 2022
+        </footer>
+     );
+}
+
+export default Footer;
