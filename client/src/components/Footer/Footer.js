a9dd44c84d2ba4dab295382f3e91b80a1c7111db
unknown <qatrelnadasouka@gmail.com>
2022-08-08 15:30:59 +0200

    add data json and words js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import "../../css/Footer/Footer.css"
+import "../../css/Footer/Footer.css";
+import { words } from './../../words';
 
 function Footer() {
     return ( 
         <footer>
-            &copy; Copyright Qatr El-Nada Souka 2022
+            {words.footerTitle}
         </footer>
      );
 }
---
+++ client/src/words.js
@@ -0,0 +1,5 @@
+export const words = {
+    headerTitle: 'MERN shopping cart',
+    footerTitle: '© Copyright Qatr El-Nada Souka 2022',
+    content: 'Content'
+}
